36495269bb9049c911e5a08a3bf37b235dbb58f5
Anand Rawool <anandrawool5624@gmail.com>
2025-01-20 20:24:28 +0530

    update

---
+++ src/main/java/org/anand/Ecom.java
@@ -0,0 +1,12 @@
+package org.anand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ecom {
+    public static void main(String[] args) {
+        SpringApplication.run(Ecom.class,args);
+        System.out.println("Ecom Started..");
+    }
+}
